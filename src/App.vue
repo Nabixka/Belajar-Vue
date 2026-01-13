fe15080c7369e8177b6256c41a1ef8216cb67c81
nabixka <nabixka05@gmail.com>
2026-01-13 07:36:37 +0700

    Fetch API & Router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
-import HelloSMK from './components/HelloSMK.vue';
 </script>
 
 <template>
-    <div class="flex gap-10 justify-center pt-10">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="w-35" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="/src/assets/vue.svg" class="w-35" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloSMK />
+    <router-view />
 </template>
 
 <style scoped>
